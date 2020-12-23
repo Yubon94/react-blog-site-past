a59d903b54514dc4042ad3fd5c00c8d984f5397c
Yu Yasuda <38551932+YasudaYu@users.noreply.github.com>
2020-12-24 05:59:13 +0900

    初期ベースの作成

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div id="container">
+        {/* menu area */}
+        <div className="menu-box">
+          <div className="icon">icon</div>
+          <nav className="menu"></nav>
+        </div>
+        {/* {content area} */}
+        <div className="content">
+          <h1>content</h1>
+          <h2></h2>
+        </div>
+      </div>
     </div>
   );
 }
